9a6f0a063c33d9bfa83cf5abec4246b53e01f126
neiro2222 <rzhechkovskii.egor@lit1533.ru>
2024-01-30 23:10:08 +0300

    1

---
+++ Base_array.cs
@@ -0,0 +1,13 @@
+using System;
+
+abstract class Base_array
+{   
+    protected abstract void _Key_init();
+    protected abstract void _Random_init();
+    public abstract void Print();
+    public abstract void Create_array(bool flag);
+    public abstract void Average();
+
+
+}
+
